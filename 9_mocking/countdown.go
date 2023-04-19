405ac8fb9f47c268ca9a93bdaadb703f3c436a10
Dmitriy Lobanov <d.lobanov5@gmail.com>
2023-04-20 01:01:27 +0300

    Lesson 9: Mocking, time.Sleep

---
+++ 9_mocking/countdown.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStartingValue = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+// We want Countdown to write data somewhere, so it can implement io.Writer
+
+func Countdown(buf io.Writer, sleeper Sleeper) {
+	for i := countdownStartingValue; i > 0; i-- {
+		fmt.Fprintln(buf, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(buf, finalWord)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
---
+++ 9_mocking/countdown_test.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+	"time"
+)
+
+// Spies are a kind of mock which can record how a dependency is used.
+// They can record the arguments sent in, how many times it has been called, etc.
+// In our case, we're keeping track of how many times Sleep() is called so we can check it in our test.
+// https://martinfowler.com/bliki/TestDouble.html
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+// assert that the sleep has been called 3 times.
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, "sleep")
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, "write")
+	return
+}
+
+const (
+	sleep string = "sleep"
+	write string = "write"
+)
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func TestCountdown(t *testing.T) {
+	t.Run(" before every print", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+
+		Countdown(buffer, &SpyCountdownOperations{})
+
+		got := buffer.String()
+		want := "3\n2\n1\nGo!"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	// In this test we're only concerned about the order of operations, so just recording them as list of named operations is sufficient.
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
+}
