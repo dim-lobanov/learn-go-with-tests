1214c81501ba6bc9fdea59627bf95084927749a7
Dmitriy Lobanov <d.lobanov5@gmail.com>
2023-04-19 23:18:13 +0300

    Lesson 8: DI, io.Writer interface

---
+++ 8_dependency_injection/greet_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{} // Buffer implements io.Writer interface
+	Greet(&buffer, "Chris")
+
+	got := buffer.String() // Buffer implements Stringer interface { String() string }
+	expected := "Hello, Chris"
+
+	if got != expected {
+		t.Errorf("got %q, want %q", got, expected)
+	}
+}
+
+// type Writer interface {
+// 	  Write(p []byte) (n int, err error)
+// }
