af5a3e5a2b5fc867274d41a17816374e76f0abf2
Dmitriy Lobanov <d.lobanov5@gmail.com>
2023-04-20 19:28:59 +0300

    Lesson 13: sync, waitgroup, sync.Mutex

---
+++ 13_sync_waitgroup/counter.go
@@ -0,0 +1,24 @@
+package counter
+
+import "sync"
+
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+// You can write just sync.Mutex in struct -> with this you can lock on like c.Lock() but it makes mutex public
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
+
+// Use channels when passing ownership of data
+// Use mutexes for managing state
