1fede7f31e7e358b77badd10775b187a33d9e1ee
Dmitriy Lobanov <d.lobanov5@gmail.com>
2023-04-22 02:01:34 +0300

    Reading files (blogposts example)

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -1,21 +1,88 @@
 package blogposts
 
-// Go 1.16 introduced an abstraction for file systems - io/fs package
+// Go 1.16 introduced an abstraction for f systems - io/fs package
 // + there is testing/fstest
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
 	"io/fs"
-	"testing/fstest"
+	"strings"
 )
 
 type Post struct {
+	Title       string
+	Description string
+	Tags        []string
+	Body        string
 }
 
-func NewPostsFromFS(fileSystem fstest.MapFS) []Post {
-	dir, _ := fs.ReadDir(fileSystem, ".")
-	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".") // TODO error
+	if err != nil {
+		return nil, err
 	}
-	return posts
+
+	var posts []Post
+	for _, f := range dir {
+		post, err := getPostFromFile(fileSystem, f.Name())
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
+
+func getPostFromFile(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	return newPost(postFile)
+}
+
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+	tagsSeparator        = "Tags: "
+)
+
+// we can change to interface Reader, because we only use io.ReadAll and don't have to couple to FS
+// Scanner provides an interface for reading data such as a file of newline-delimited lines of text.
+func newPost(postFile io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postFile) // note that NewScanner accepts io.Reader
+
+	// separating what from how by extracting this func
+	readLine := func() string {
+		scanner.Scan()        // returns next token or false
+		return scanner.Text() // returns a new string from the latest token from scanner
+		// string is newly allocated and holds it's bytes
+	}
+
+	title := readLine()[len(titleSeparator):]
+	description := readLine()[len(descriptionSeparator):]
+	tagsLine := readLine()[len(tagsSeparator):]
+	var tags []string = strings.Split(tagsLine, ", ")
+	body := readBody(scanner)
+
+	return Post{
+		Title:       title,
+		Description: description,
+		Tags:        tags,
+		Body:        body,
+	}, nil
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan()        // ignore a body separator line
+	buf := bytes.Buffer{} // struct with bytes array, offset int and last read operation for Unread!
+	for scanner.Scan() {  // Scan() returns a bool, so we can iterate until EOF (or error)
+		fmt.Fprintln(&buf, scanner.Text()) // Buffer{} implements io.Writer
+	}
+	return strings.TrimSuffix(buf.String(), "\n") // remove last new line symbol
 }
--- blogposts/test/blogposts_test.go
+++ blogposts/test/blogposts_test.go
@@ -1,25 +1,78 @@
 package blogpost_test
 
 import (
+	"errors"
+	"io/fs"
+	"learn-go-with-tests/blogposts"
+	"reflect"
 	"testing"
 	"testing/fstest"
-	"learn-go-with-tests/blogposts" 
 	// <modulename>/<relative-filepath-from-module-root>
 	// directory name has to correspond to the package name for us to import it
 )
 
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, i always fail")
+}
+
+func TestNewBlogPostsFail(t *testing.T) {
+	_, err := blogposts.NewPostsFromFS(StubFailingFS{})
+	if err == nil {
+		t.Error("wanted error during file opening, but didn't get one")
+	}
+}
+
 func TestNewBlogPosts(t *testing.T) {
-	// A MapFS is a simple in-memory file system for use in tests, represented as a map from 
-	// path names (arguments to Open) to information about the files or directories they represent.
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+B
+L
+M`
+	)
+
 	fs := fstest.MapFS{
-		"hello_world.md": {Data: []byte("hi")},
-		"hello_world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
 	}
 
-	posts := blogposts.NewPostsFromFS(fs)
-
-	if len(posts) != len(fs) {
-		t.Errorf("got %d posts, want %d", len(posts), len(fs))
+	posts, err := blogposts.NewPostsFromFS(fs)
+	if err != nil {
+		t.Fatal(err)
 	}
 
+	// rest of test code cut for brevity
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+World`,
+	})
+	assertPost(t, posts[1], blogposts.Post{
+		Title:       "Post 2",
+		Description: "Description 2",
+		Tags:        []string{"rust", "borrow-checker"},
+		Body: `B
+L
+M`,
+	})
+}
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
 }
